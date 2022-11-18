77ca7adabd8ac86ead714c2ed4f0043cb5132489
Nikos <alknetor@msn.com>
2022-11-18 13:30:29 +0200

    Added simple layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,26 @@
-<script setup>
-import { RouterLink, RouterView } from "vue-router";
-</script>
-
 <template>
-  <header>
-    <div class="wrapper">
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
+  <v-app>
+    <v-app-bar
+      >v-app-bar-area
+      <RouterLink class="router-link" to="/">Home</RouterLink>
+      <RouterLink class="router-link" to="/about">About</RouterLink>
+    </v-app-bar>
 
+    <v-navigation-drawer>v-navigation-drawer area</v-navigation-drawer>
+
+    <v-main>
+      <v-container>Content area</v-container>
+    </v-main>
+
+    <v-bottom-navigation>v-bottom-navigator</v-bottom-navigation>
+  </v-app>
   <RouterView />
 </template>
+
+<script>
+import { RouterLink, RouterView } from "vue-router";
+
+export default {};
+</script>
 
 <style scoped></style>
