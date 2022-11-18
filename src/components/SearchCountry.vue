61a51a19a99bcb7e877b4c24358445d3a13f80be
Nikos <alknetor@msn.com>
2022-11-18 15:50:01 +0200

    Created search country component

---
+++ src/components/SearchCountry.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="col">
+    <h2>Start by searching for a country</h2>
+
+    <div class="search-country">
+      <v-text-field
+        clearable
+        label="Search for a country"
+        variant="underlined"
+        v-model="countryText"
+      ></v-text-field>
+      <v-btn @click="searchCountry" class="btn" color="blue">Search</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+import { geocodingAPI } from "../services/geocodingAPI";
+
+export default {
+  data() {
+    return {
+      countryText: "",
+    };
+  },
+
+  methods: {
+    async searchCountry() {
+      res = await geocodingAPI.searchByCity(this.countryText, "metric");
+    },
+  },
+};
+</script>
+
+<style lang="css" scoped>
+.col {
+  display: flex;
+  flex-direction: column;
+  flex-wrap: nowrap;
+  align-items: center;
+  justify-content: center;
+}
+.search-country {
+  display: flex;
+  flex-direction: row;
+  flex-wrap: nowrap;
+  align-items: center;
+  width: 500px;
+}
+
+.btn {
+  margin-left: 10px;
+}
+</style>
