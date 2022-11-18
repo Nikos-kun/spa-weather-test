36fd1d594f3b178585f16f3253a2b45a985ba6ff
Nikos <alknetor@msn.com>
2022-11-18 20:24:18 +0200

    Created new pinia store for countries

---
+++ src/stores/countries.js
@@ -0,0 +1,29 @@
+import { defineStore } from "pinia";
+
+import { geocodingAPI } from "../services/geocodingAPI";
+
+export const useCountriesStore = defineStore("countriesStore", {
+  state: () => ({
+    countries: [],
+  }),
+
+  getters: {
+    getCountries: (state) => state.countries,
+  },
+
+  actions: {
+    setCountries(country, units) {
+      return new Promise(async (resolve, reject) => {
+        try {
+          let data = await geocodingAPI.searchByCity(country, units);
+
+          if (data && data.length >= 0) {
+            this.countries = data;
+          }
+        } catch (error) {
+          console.error("Something went wrong in setCountries()", error);
+        }
+      });
+    },
+  },
+});
